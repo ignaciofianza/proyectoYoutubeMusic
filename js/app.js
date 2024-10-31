9f8ea4c7e835888d69aa25742cf8aa673b5ce385
Ignacio Fianza <ignacio@ignaciofianza.com>
2024-10-31 20:12:35 -0300

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,3 @@
+window.onload = function() {
+    alert("Aviso importante:\n\nEsta página es un mockup de la plataforma YouTube, creado únicamente para fines demostrativos y sin ningún propósito comercial. Ni esta página ni su código están destinados a ser utilizados con otros fines. Los autores no se hacen responsables de cualquier uso adicional de la página y/o del código por parte de terceros.\n\nGracias por su comprensión.");
+};
